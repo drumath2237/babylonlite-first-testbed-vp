88a372873cb7fca342566a9f1606bc1a4b242c1f
drumath2237 <2237k.drumath@gmail.com>
2026-06-21 13:40:13 +0900

    :tada: first scene setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,39 @@
+import {
+  addToScene,
+  attachControl,
+  createBox,
+  createDefaultCamera,
+  createDirectionalLight,
+  createEngine,
+  createSceneContext,
+  createStandardMaterial,
+  registerScene,
+  startEngine,
+} from "@babylonjs/lite";
 import "./style.css";
 
-function main() {
-  console.log("Hello");
+async function main() {
+  const renderCanvas = document.getElementById("renderCanvas");
+  if (!(renderCanvas instanceof HTMLCanvasElement)) {
+    return;
+  }
+
+  const engine = await createEngine(renderCanvas);
+  const scene = createSceneContext(engine);
+
+  const camera = createDefaultCamera(scene);
+  attachControl(camera, renderCanvas, scene);
+
+  const light = createDirectionalLight([-0.5, -1, 2]);
+  addToScene(scene, light);
+
+  const box = createBox(engine, 0.3);
+  // box.position.set(0, 0, 1);
+  box.material = createStandardMaterial();
+  addToScene(scene, box);
+
+  await registerScene(scene);
+  await startEngine(engine);
 }
 
-main();
+await main();
